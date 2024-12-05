5a4ebc6186c9c6f9ea80eabd4e2e7b3a36e59bf0
Tak1za <varungupta2015135@gmail.com>
2024-12-05 18:26:37 +0530

    added sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import "./App.css";
+import { ModeToggle } from "@/components/mode-toggle";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <>
-      <div>
-        <Button>Click me</Button>
-      </div>
-      <ModeToggle />
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full pt-2 pr-2 pb-2">
+          <div className="flex flex-row items-center justify-between">
+            <SidebarTrigger />
+            <ModeToggle />
+          </div>
+        </main>
+      </SidebarProvider>
     </>
   );
 }
